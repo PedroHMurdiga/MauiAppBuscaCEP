d006ead4091952d26c4818a2f9b51045d0cb6d02
Usuario <Usuario@CPD03-M14>
2024-05-13 11:13:32 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppBuscaCEP/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppBuscaCEP
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiAppBuscaCEP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppBuscaCEP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
