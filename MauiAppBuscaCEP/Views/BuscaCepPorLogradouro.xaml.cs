80927cff32cf7a3447b9f5a51a9afd71b4a82a1e
Usuario <Usuario@CPD03-M14>
2024-06-03 11:12:44 -0300

    Finalização do projeto

--- MauiAppBuscaCEP/Views/BuscaCepPorLogradouro.xaml.cs
+++ MauiAppBuscaCEP/Views/BuscaCepPorLogradouro.xaml.cs
@@ -1,9 +1,34 @@
+using MauiAppBuscaCEP.Models;
+using MauiAppBuscaCEP.Services;
+
 namespace MauiAppBuscaCEP.Views;
 
 public partial class BuscaCepPorLogradouro : ContentPage
 {
-	public BuscaCepPorLogradouro()
-	{
-		InitializeComponent();
-	}
+    public BuscaCepPorLogradouro()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            carregando.IsRunning = true;
+
+            List<Cep> arr_ceps =
+                await DataService.GetCepByLogradouro(
+                    txt_logradouro.Text);
+
+            lst_ceps.ItemsSource = arr_ceps;
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+        finally
+        {
+            carregando.IsRunning = false;
+        }
+    }
 }
--- MauiAppBuscaCEP/Views/Menu.xaml.cs
+++ MauiAppBuscaCEP/Views/Menu.xaml.cs
@@ -6,4 +6,15 @@ public partial class Menu : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(
+            new Views.BuscaCepPorLogradouro());
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new MainPage());
+
+    }
 }
